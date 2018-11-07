59be20014a92b406f97374b21293a9d7f6441fb7
leiqiang123 <1229949407@qq.com>
2018-11-07 21:50:27 +0800

    update

--- test.js
+++ test.js
@@ -1,7 +1,17 @@
-(function(){
-    console.log(1111)
-})()
+// (function(){
+//     console.log(1111)
+// })()
 
-;(function(){
-    console.log(2222)
-})()
+// ;(function(){
+//     console.log(2222)
+// })()
+
+
+var arr = [1, 2, 3]
+
+var arr1 = arr.filter(item => {
+    return item < 3
+})
+
+console.log(arr)
+console.log(arr1)
